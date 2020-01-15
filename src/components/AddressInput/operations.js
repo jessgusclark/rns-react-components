96949bb3d50248cd36af27a144b354b37c87cfd5
Jesse Clark <hello@developerjesse.com>
2020-01-15 14:29:42 +0200

    Add domain check.

--- src/components/AddressInput/operations.js
+++ src/components/AddressInput/operations.js
@@ -1,15 +1,17 @@
 import Web3 from 'web3';
-import { provider } from '../../web3Provider';
+
 import {
   startAddressValidation,
   returnValidAddress,
   finishAddressValidation,
 } from './actions';
 
+
 export default (dispatch, addr) => {
   dispatch(startAddressValidation(addr));
 
-  const web3 = new Web3(provider());
+  // a provicer is not needed since we are not accessing the network
+  const web3 = new Web3(null);
 
   // check if the address is a valid address:
   if (web3.utils.isAddress(addr)) {
@@ -17,6 +19,14 @@ export default (dispatch, addr) => {
     return dispatch(finishAddressValidation());
   }
 
+  // check to see if the input is a domain:
+  const domain = addr.endsWith('.rsk') ? addr : `${addr}.rsk`;
+
+  // mocked!
+  if (domain === 'jesse.rsk') {
+    dispatch(returnValidAddress('0x12345678901234567890'));
+    return dispatch(finishAddressValidation());
+  }
 
   return dispatch(finishAddressValidation());
 };
