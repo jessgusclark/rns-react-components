c5451142db0d63a6655921669013168ae70242d2
Jesse Clark <hello@developerjesse.com>
2020-01-15 14:16:31 +0200

    Return adddress and finish if input is a valid address.

--- src/components/AddressInput/operations.js
+++ src/components/AddressInput/operations.js
@@ -1,12 +1,22 @@
+import Web3 from 'web3';
+import { provider } from '../../web3Provider';
 import {
   startAddressValidation,
   returnValidAddress,
+  finishAddressValidation,
 } from './actions';
 
 export default (dispatch, addr) => {
-  console.log('operations.js hit');
-
   dispatch(startAddressValidation(addr));
-  dispatch(returnValidAddress('tacotaco!'));
 
+  const web3 = new Web3(provider());
+
+  // check if the address is a valid address:
+  if (web3.utils.isAddress(addr)) {
+    dispatch(returnValidAddress(addr));
+    return dispatch(finishAddressValidation());
+  }
+
+
+  return dispatch(finishAddressValidation());
 };
--- src/components/AddressInput/view/AddressInputComponent.js
+++ src/components/AddressInput/view/AddressInputComponent.js
@@ -21,7 +21,7 @@ const AddressInputComponent = ({searchButtonClick, searchAddress, returnAddress,
       </button>
 
       <hr />
-      <h2>Debug Log</h2>
+      <h2>Debug Log & Test Section</h2>
       <ul>
         <li>
           Search Address:
@@ -36,7 +36,10 @@ const AddressInputComponent = ({searchButtonClick, searchAddress, returnAddress,
           {returnAddress}
         </li>
       </ul>
-
+      <hr />
+      <p>
+        Valid Adddress to copy: 0x3Dd03d7d6c3137f1Eb7582Ba5957b8A2e26f304A
+      </p>
     </div>
   );
 };
